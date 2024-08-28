76ee10831b9dcaccf43ae475be4b5e01198a7019
Classiee <Kristianfratini@gmail.com>
2024-08-28 11:10:42 +0200

    Update version 2
    Team 3 son finpusser og stusser!

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const washBar = document.getElementById('washColor');
 const foodBar = document.getElementById('healthColor');
 const playBar = document.getElementById('playColor');
+let textContainer = document.getElementById('textContainer')
+let imageContainer = document.getElementById('imageContainer')
+let changeText = document.getElementById('text')
 var foodProgress = 100;
 var washProgress = 100;
 var playProgress = 100;
@@ -20,67 +23,175 @@ function decay() {
     washProgress -= 5;
     playProgress -= 5;
 
-if (foodProgress <= 30) {
+if (foodProgress == 30) {
     badHealth();
     foodBar.style.backgroundColor = 'red';
 }
-if (washProgress <= 30) {
+
+if (washProgress == 30) {
     badHealth();
     washBar.style.backgroundColor = 'red';
 }
-if (playProgress <= 30){
+if (playProgress == 30){
     badHealth();
     playBar.style.backgroundColor = 'red';
 }
-if (foodProgress == -5){
-    dead();
+
+
+
+
+if (foodProgress == 45) {
+    medHealthMinus();
+    foodBar.style.backgroundColor = 'orange';
 }
-if (washProgress == -5) {
+if (washProgress == 45) {
+    medHealthMinus();
+    washBar.style.backgroundColor = 'orange';
+}
+if (playProgress == 45){
+    medHealthMinus();
+    playBar.style.backgroundColor = 'orange';
+}
+/**/
+
+if (playProgress == 0 || washProgress == 0 || foodProgress == 0) {
+    textContainer.innerHTML += `<li> Ahhhhh, jeg døde! </li><br>`;
+}
+
+if (playProgress == 30 || washProgress == 30 || foodProgress == 30) {
+    textContainer.innerHTML += `<li> Hjelp, jeg dør! </li><br>`;
+}
+
+if (playProgress == 50) {
+    textContainer.innerHTML += `<li> Lek med meg! </li><br>`;
+}
+
+if (washProgress == 50) {
+    textContainer.innerHTML += `<li> Jeg trenger en runde med hageslangen! </li><br>`;
+}
+ 
+if (foodProgress == 50) {
+    textContainer.innerHTML += `<li> Jeg er sulten, gi meg noen kyllingvinger! </li><br>`;
+}
+
+if (playProgress == 80) {
+    textContainer.innerHTML += `<li> Kan man dø av kjedsomhet?! </li><br>`;
+}
+
+if (washProgress == 80) {
+   textContainer.innerHTML += `<li> Jeg trenger en kattevask! </li><br>`;
+}
+
+if (foodProgress == 80) {
+    textContainer.innerHTML += `<li> Jeg kunne gjerne spist et egg nå! </li><br>`;
+}
+
+
+if (foodProgress == 70) {
+    badHealth();
+    foodBar.style.backgroundColor = 'yellow';
+}
+
+if (washProgress == 70) {
+    badHealth();
+    washBar.style.backgroundColor = 'yellow';
+}
+if (playProgress == 70){
+    badHealth();
+    playBar.style.backgroundColor = 'yellow';
+}
+
+
+if (foodProgress == -10 || washProgress == -10 || playProgress == -10){
     dead();
 }
 
-if (playProgress == -5){
-    dead();  
-}
+// For å redusere IF statements, og i tillegg gjøre funksjonen mer presis og pålitelig
+// (Kristian's input, NOT TO USE!)
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+// function updateProgressBar(bar, progress, type) {
+//     if (type === 'height') {
+//         bar.style.height = progress + '%';
+//     } else if (type === 'width') {
+//         bar.style.width = progress + '%';
+//     }
+
+//     if (progress <= 30) {
+//         badHealth();
+//         bar.style.backgroundColor = 'red';
+//     } else if (progress == 45) {
+//         medHealthMinus();
+//         bar.style.backgroundColor = 'orange';
+//     } else if (progress == 70) {
+//         bar.style.backgroundColor = 'yellow';
+//     }
+// }
+
+// function decay() {
+//     updateProgressBar(washBar, washProgress, 'height');
+//     updateProgressBar(playBar, playProgress, 'height');
+//     updateProgressBar(foodBar, foodProgress, 'width');
+
+//     foodProgress -= 5;
+//     washProgress -= 5;
+//     playProgress -= 5;
+
+//     if (foodProgress <= -10 || washProgress <= -10 || playProgress <= -10) {
+//         dead();
+//     }
+// }
    
 }
 function hungry() {
-    document.getElementById('textContainer').innerHTML += '<li>Du har mada dyret! </li>';
-    foodBar.style.backgroundColor = 'rgb(236, 78, 78)';
+    textContainer.innerHTML += '<li>Du har mada dyret! </li>';
+    foodBar.style.backgroundColor = 'lightgreen';
     foodProgress = 100;
     goodHealth();
 }
 
 function playMe() {
-    document.getElementById('textContainer').innerHTML += `<li> Du har lekt med dyret! </li>`;
-    playBar.style.backgroundColor = 'lightyellow;' 
+   textContainer.innerHTML += `<li> Du har lekt med dyret! </li>`;
+    playBar.style.backgroundColor = 'lightgreen' 
     playProgress = 100;
     goodHealth();
 
 }
 function dirty(){
-    document.getElementById('textContainer').innerHTML += `<li> Du har vaska dyret! </li>`;
-    washBar.style.backgroundColor = 'lightblue';
+    textContainer.innerHTML += `<li> Du har vaska dyret! </li>`;
+    washBar.style.backgroundColor = 'lightgreen';
     washProgress = 100;
     goodHealth();
 }
 
 function goodHealth(){
-    document.getElementById('imageContainer').innerHTML = `
+    imageContainer.innerHTML = `
     <img id="pet" src="/img/happyChicken.png" alt="Pet">
     `;    
-  
-}
+    }
+/*function medHealth() {
+      imageContainer.innerHTML = `
+        <img id=pet src="img/SleepingChicken.png" alt="Pet">`;
+    }
+    function medHealthMinus() {
+        imageContainer.innerHTML = `
+        <img id=pet src="img/SleepingHungryChicken.png" alt="Pet">`;
+    }
+*/
 function badHealth(){
-    document.getElementById('imageContainer').innerHTML = `
+    imageContainer.innerHTML = `
     <img id="pet" src="img/MadChicken.png" alt="Pet">
     `;    
    
 }
 function dead(){
-    document.getElementById('imageContainer').innerHTML = `
+    changeText.innerHTML = 'Døde'
+   imageContainer.innerHTML = `
     <img id="pet" src="img/deadChicken.png" alt="Pet">
     `;    
 
     clearInterval(minTimer);
+    // if (confirm("Dyret ble borte, vil du prøve på nytt?")==true) {
+    //     location.reload();
+    // }
 }
